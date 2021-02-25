64ec684ac99a22b70561ac2ea88b9696652db253
morimori8282 <moriwaki8282@yahoo.co.jp>
2021-02-25 09:35:10 +0900

    make count up app

---
+++ readme.txt
@@ -0,0 +1,1 @@
+count up for swift
--- swift_count_up/ViewController.swift
+++ swift_count_up/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var count_label: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        count_label.text = String(count)
     }
 
-
+    @IBAction func plus_button(_ sender: Any) {
+        count += 1
+        count_label.text = String(count)
+        
+        if count >= 10 {
+            count_label.textColor = .yellow
+        }
+        
+    }
+    @IBAction func minus_button(_ sender: Any) {
+        count -= 1
+        
+        if count < 0 {
+            count = 0
+        }
+        count_label.text = String(count)
+        
+        if count < 10 {
+            count_label.textColor = .white
+        }
+    }
+    
 }
 
